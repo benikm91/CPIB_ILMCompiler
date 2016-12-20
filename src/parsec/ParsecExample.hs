4d5999d3f21db1f4eb831c411661c6a5943856f7
Christian Scheller <ch.scheller@hotmail.com>
2016-12-20 17:12:37 +0100

    some parsers

---
+++ src/parsec/ParsecExample.hs
@@ -0,0 +1,82 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+
+readExpr :: String -> IMLVal
+readExpr input = case parse parseProgram "Hambbe" input of
+    Left err -> Message $ "fuck you: " ++ show err
+    Right val -> val
+
+data IMLVal = Program IMLVal
+            | Ident String
+            | Message String
+            deriving Show
+
+
+spaces :: Parser ()
+spaces = skipMany space
+
+spaces1 :: Parser ()
+spaces1 = skipMany1 space
+
+identStartChars :: String
+identStartChars = ['a'..'z']++['A'..'Z']++"_"
+
+identChars :: String
+identChars = identStartChars++['0'..'9']
+
+parseProgram :: Parser IMLVal
+parseProgram = parseProgram1
+
+parseProgram1 :: Parser IMLVal
+parseProgram1 = do
+    string "program"
+    spaces
+    name <- parseIdent 
+    spaces
+    char '{'
+    spaces 
+    x <- many (noneOf "}")
+    spaces
+    char '}'
+    return $ Program name
+
+progParamList :: Parse IMLVal
+progParamList = do
+    char '('
+    spaces
+
+progParam
+
+_progParamOptional :: Parse IMLVal
+_progParamOptional = do
+
+
+	(program,
+		[[T PROGRAM, T IDENT, N progParamList, T LBRACE, N cpsCmd, T RBRACE],
+		 [T PROGRAM, T IDENT, N progParamList, T GLOBAL, N cpsDecl, T LBRACE, N cpsCmd, T RBRACE]]),
+
+	(progParamList, 
+		[[T LPAREN, N underlineprogParamOptional, T RPAREN]]),
+        
+	(underlineprogParamOptional, 
+		[[N progParam, N underlineprogParamMany], 
+
+        
+	(progParam, 
+		[[N underlineflowModeOptional, N underlinechangeModeOptional, T typedIdent]]),
+
+parseIdent :: Parser IMLVal
+parseIdent = do
+                head <- oneOf identStartChars
+                tail <- many $ oneOf identChars
+                return $ Ident (head : tail)
+
+mustafa2 :: String
+mustafa2 = "Hambbe ist der König dieser Welt!"
+
+mustafa3 :: IMLVal
+mustafa3 = readExpr "program HambbeKoenig {}"
+
+main :: IO()
+main = print . readExpr $ "program HambbeKoenig {}"

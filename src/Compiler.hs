212f9457f6804d176dad5828df88df79b57a7372
Tobias Bollinger <Tobias Bollinger>
2016-12-29 18:19:12 +0100

    asdasd

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -5,13 +5,8 @@ module Compiler
 import Parser
 
 readAndCompile :: IO ()
-<<<<<<< HEAD
-readAndCompile = do  
-    program <- readFile "D:/OneDrive/Dokumente/FHNW-MightyTower/cpib/CPIB_ILMCompiler/sample/sample1.iml"
-=======
 readAndCompile = do
-    program <- readFile "/Users/benikm91/Documents/FHNW/Semester5/cpib/MyStuff/compiler/sample/sample1.iml"
->>>>>>> 9a7d6a754bca17830e71f11866e004a683ec069f
+    program <- readFile "D:/OneDrive/Dokumente/FHNW-MightyTower/cpib/CPIB_ILMCompiler/sample/sample1.iml"
     -- "/Users/benikm91/Documents/FHNW/Semester5/cpib/MyStuff/compiler/sample/sample1.iml"
     -- "E:/Users/Christian/Documents/FHNW/Semester5/cpib/CPIB_ILMCompiler/sample/sample1.iml"
     -- "D:/OneDrive/Dokumente/FHNW-MightyTower/cpib/CPIB_ILMCompiler/sample/sample1.iml"

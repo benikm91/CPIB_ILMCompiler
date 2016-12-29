0fc778414bd4150483f18631603707b130214f15
Tobias Bollinger <Tobias Bollinger>
2016-12-29 20:52:49 +0100

    Added Path

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -3,10 +3,11 @@ module Compiler
     ) where
 
 import Parser
+import Path
 
 readAndCompile :: IO ()
 readAndCompile = do
-    program <- readFile "D:/OneDrive/Dokumente/FHNW-MightyTower/cpib/CPIB_ILMCompiler/sample/sample1.iml"
+    program <- readFile getFilePath
     -- "/Users/benikm91/Documents/FHNW/Semester5/cpib/MyStuff/compiler/sample/sample1.iml"
     -- "E:/Users/Christian/Documents/FHNW/Semester5/cpib/CPIB_ILMCompiler/sample/sample1.iml"
     -- "D:/OneDrive/Dokumente/FHNW-MightyTower/cpib/CPIB_ILMCompiler/sample/sample1.iml"

7821f4df600975d79f2357ab45c8c86fae081540
benikm91 <benikm91@gmail.com>
2016-12-30 16:06:30 +0100

    Working example with Int32

--- src/VMSample.hs
+++ src/VMSample.hs
@@ -1,17 +1,20 @@
 module VMSample ( run ) where
 
+import BaseDecls
 import VirtualMachineIO
 import Locations
 import Data.Array
 import Data.Monoid
+import CheckedArithmetic
+import Data.Either.Utils
 
 import ErrorHandlingGeneric(Loc, ErrorMsgGen, CheckGen, CheckerGen)
 
 run :: IO ()
 run = do 
-    t <- debugProgram ("HambbeToni", program)
+    t <- execProgram ("HambbeToni", program)
     print t
     return ()
 
 program :: (Array Int Instruction)
-program = array (0, 3) [(0, LoadIm IntVmTy (IntVmVal 5)), (1, LoadIm IntVmTy (IntVmVal 4)), (2, Add IntVmTy (rc2loc (1, 1))), (3, Stop)]
+program = array (0, 4) [(0, LoadIm Int32VmTy (Int32VmVal (fromRight $ fromIntegerToInt32 5))), (1, LoadIm Int32VmTy (Int32VmVal (fromRight $ fromIntegerToInt32 4))), (2, Add Int32VmTy mempty), (3, Output (IntTy (32 :: Int)) "HALLO"), (4, Stop)]

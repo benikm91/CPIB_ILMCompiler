b3f9aa4896c3fd052e8d742e223a2f3b6c68bbe0
Tobias Bollinger <Tobias Bollinger>
2017-01-02 14:34:12 +0100

    Added updatePcSp function.

--- src/CodeGenerator.hs
+++ src/CodeGenerator.hs
@@ -100,6 +100,10 @@ updatePc (pc, sp, global, locals) i = (pc + i, sp, global, locals)
 updateSp :: Enviroment -> Int -> Enviroment
 updateSp (pc, sp, global, locals) i = (pc, sp + i, global, locals)
 
+-- env pc sp 
+updatePcSp :: Enviroment -> Int -> Int -> Enviroment
+updatePcSp (pc, sp, global, locals) i k = (pc + i, sp + k, global, locals)
+
 addLocalIdent :: Enviroment -> Ident -> Enviroment
 -- TODO Check if Ident already exists => Throw error
 addLocalIdent (pc, sp, global, locals) ident = (pc, sp + 1, global, addToLocalScope locals ident)
@@ -237,21 +241,21 @@ connectCode (instructions, env) statement = (instructions ++ newInstructions, ne
     where (newInstructions, newEnv) = generateCode statement env
 
 generateCode :: IMLVal -> Enviroment -> ([Instruction], Enviroment)
-generateCode (Ident name) env = ([loadAddrRel $ getIdentAddress env name, deref ], updateSp (updatePc env 2) 1)
-generateCode (IdentArray (Ident name) i) env = ([loadAddress arrayAddres, deref ], updatePc env 2)
+generateCode (Ident name) env = ([loadAddrRel $ getIdentAddress env name, deref ], updatePcSp 2 1)
+generateCode (IdentArray (Ident name) i) env = ([loadAddress arrayAddres, deref ], updatePcSp 2 1)
     where startAddress =  getIdentAddress env name
           ident@(_, add, identInfo) = getIdent env name
           imlType = extractImlType identInfo
           amax = extractArrayMax imlType
           amin = extractArrayMin imlType
           arrayAddres = getArrayAddress i amin amax startAddress
-generateCode (Literal (IMLInt i)) env = ([loadIm32 $ toInteger i], updateSp (updatePc env 1) 1)
+generateCode (Literal (IMLInt i)) env = ([loadIm32 $ toInteger i], updatePcSp 1 1)
 generateCode (MonadicOpr Parser.Minus expression) env = (expressionInstructions ++ [neg], updatePc newEnv 1)
     where (expressionInstructions, newEnv) = generateCode expression env
 generateCode (Assignment imlIdent@(Ident name) expression) env = generateAssignmentCode imlIdent (thd3 $ getIdent env name) (generateCode expression env)
 generateCode (Assignment imlIdent@(IdentArray (Ident name) _) expression) env = generateAssignmentCode imlIdent (thd3 $ getIdent env name) (generateCode expression env)
 generateCode (IdentFactor ident Nothing) env = generateCode ident env
-generateCode (DyadicOpr op a b) env = (expressionInstructions ++ [getDyadicOpr op], updateSp (updatePc newEnv 1) (-1))
+generateCode (DyadicOpr op a b) env = (expressionInstructions ++ [getDyadicOpr op], updatePcSp 1 -1)
     where (expressionInstructions, newEnv) = (fst (generateCode a env) ++ fst (generateCode b env), snd $ generateCode b (snd $ generateCode a env))
 generateCode (If condition ifStatements elseStatements) env@(_, _, global, locals) = (conditionInstructions ++ [condJump (getPc ifEndEnv + 1)] ++ ifStatementInstructions ++ [uncondJump (getPc elseEndEnv + 1)] ++ elseStatementInstructions, elseEndEnv)
     where (conditionInstructions, condEndEnv) = generateCode condition env
@@ -278,15 +282,15 @@ handleNext (IdentFactor (Ident name) _, (_, _, Param _ InOut changeMode)) env@(_
 handleNext _ env = ([], updateSp env (-1))
 
 generateIdentDeclarationCode :: String -> IMLChangeMode -> IMLType -> Enviroment -> ([Instruction], Enviroment)
-generateIdentDeclarationCode name changeMode Int env = ([loadIm32 0], updateSp (updatePc newEnv 1) 1)
+generateIdentDeclarationCode name changeMode Int env = ([loadIm32 0], updatePcSp 1 1)
     where newEnv = addLocalIdent env (name, getSp env, CodeGenerator.Var Int changeMode)
 generateIdentDeclarationCode name changeMode var@(ClampInt cmin cmax) env 
     | cmax <= cmin = error "Max of Clamp must be greater than min"
-    | otherwise = ([loadIm32 $ toInteger cmin], updatePc newEnv 1)
+    | otherwise = ([loadIm32 $ toInteger cmin], updatePcSp newEnv 1 1)
     where newEnv = addLocalIdent env (name, getSp env, CodeGenerator.Var var changeMode)
 generateIdentDeclarationCode name changeMode var@(ArrayInt amin amax) env
     | amax <= amin = error "Max of Array must be greater than min"
-    | otherwise = (instructions, updatePc newEnv (amax - amin))
+    | otherwise = (instructions, updatePcSp newEnv (amax - amin) (amax - amin))
     where instructions = generateIdentDeclarationArrayCode amin amax
           newEnv = addLocalIdent env (name, getSp env, CodeGenerator.Var var changeMode)
 
